77140f19599326963b943b17661d95a43990b72c
c1gyoden <c1gyoden@vasvari.org>
2024-09-17 11:14:17 +0200

    ááááááááááááá

---
+++ 3mal-osztható-számok-20ig/3mal-osztható-számok-20ig/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3mal_osztható_számok_20ig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                if (i % 3 == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
